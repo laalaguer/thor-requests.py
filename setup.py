5222dd45e64e0a5a62ed5800fe02404a578e75c3
laalaguer <laalaguer@gmail.com>
2021-03-31 15:04:41 +0800

    release 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ assert long_description
 
 setuptools.setup(
     name="thor-requests",
-    version="0.1.2",
+    version="0.1.3",
     author="laalaguer",
     author_email="laalaguer@gmail.com",
     description="VeChain for humans",
--- tests/README.md
+++ tests/README.md
@@ -1,1 +1,5 @@
 Test and examples
+
+You should be able to connect to a running VeChain node to perform tests.
+
+eg. 'http://testnet.veblocks.net'
