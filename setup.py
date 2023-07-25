b1f5517c5f59db2a7aff303683a21f98ced8ec21
laalaguer <laalaguer@gmail.com>
2023-07-25 17:08:05 +0800

    Update Version. Remove Pysha3 dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ assert long_description
 
 setuptools.setup(
     name="thor-requests",
-    version="1.2.4",
+    version="1.2.5",
     author="laalaguer",
     author_email="laalaguer@gmail.com",
     description="Simple network VeChain SDK for human to interact with the blockchain",
